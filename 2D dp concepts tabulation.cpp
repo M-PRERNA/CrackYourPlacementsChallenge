e8f47c81a624a03f738d4505b5f0333d700e2c49
PRERNA MISHRA <mprerna802@gmail.com>
2022-06-01 20:00:00 +0530

    Update 2D dp concepts tabulation.cpp

--- 2D dp concepts tabulation.cpp
+++ 2D dp concepts tabulation.cpp
@@ -1,24 +1,3 @@
-int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp){
-    if(day == 0){
-        int maxi = 0;
-        for (int task=0; task<3; task++){
-            if(task != last){
-                maxi = max(maxi, points[0][task]);
-            }
-        }
-        return dp[day][last] = maxi;
-    }
-    if (dp[day][last] != -1) return dp[day][last];
-    int maxi = 0;
-    for (int task=0; task<3; task++){
-        if(task!=last){
-            int point = points[day][task] + f(day-1, task, points, dp);
-            
-            maxi = max(point, maxi);
-        }
-    }
-    return dp[day][last] = maxi;
-}
 
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
