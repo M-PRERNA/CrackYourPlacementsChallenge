3920e99556267628e29f91fca2f1a6513e5342bc
PRERNA MISHRA <mprerna802@gmail.com>
2022-06-12 20:41:11 +0530

    Create longest pallindromic subsequence.cpp

---
+++ longest pallindromic subsequence.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+int lcs(string &s, string &t)
+{
+    vector<int> curr (t.size(), 0);
+    vector<int> prev (t.size(), 0);
+    for(int j = 0; j<t.size(); j++){
+        if(s[0] == t[j]) prev[j] = 1;
+    }
+    
+    for(int i = 1; i<s.size(); i++){
+        for(int j = 1; j<t.size(); j++){
+           if(s[i] == t[j]) curr[j] = 1 + prev[j-1];
+            
+           else curr[j] = 0 + max(prev[j], curr[j-1]);
+        }
+        prev = curr;
+    }
+
+    return prev[t.size()-1];
+}
+
+string longestPalinSubstring(string str)
+{
+    // Write your code here.
+    string t = str;
+    reverse(t.begin(), t.end());
+    return lcs(str, t);
+}
