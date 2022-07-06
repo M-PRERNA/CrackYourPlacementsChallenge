b500774f632c17e55b2fb24c0a77d79dda3441fa
PRERNA MISHRA <mprerna802@gmail.com>
2022-07-06 12:06:36 +0530

    Update adjacency list._template.cpp

--- interview_graphs/adjacency list._template.cpp
+++ interview_graphs/adjacency list._template.cpp
@@ -3,6 +3,35 @@ using namespace std;
 int main(){
   int n, m;
   cin >>n>>m;
+//   m is the number of edges --- n is the number of nodes
+//  a----b----c
+//  |          \
+//  f-----g-----h
+//  |           |
+//  d----e------k   here m = 10 and n=9
+//   the loop will look like: 
+// [m:  u-----v]
+//  0:  a ----b
+//  1:  b-----c
+//  2:  c-----h
+//  3:  a------f
+//  4:  f------g
+//  5:  g------h
+//  6:  h------k
+//  7:  e------k
+//  8:  f------d
+//  9:  d-------e
+  
+//   adjacency list:
+//   [a]:(b, f)
+//   [b]:(a, c)
+//   [c]:(b, h)
+//   [d]:(f, e)
+//   [e]:(k, d)
+//   [f]:(g, d)
+//   [g]:(f, h)
+//   [h]:(g, k)
+//   [k]:(e, h)
   vector<int> adj[n+1]; //array of adjacency lists
 //vector<pair<int, int>> adj[n+1];
   for(int i = 0; i<m; i++){
