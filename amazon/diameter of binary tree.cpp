8b4fc6aace6eacbd9ee673d7fe4f2e52805bd2f7
PRERNA MISHRA <mprerna802@gmail.com>
2022-08-24 19:17:57 +0530

    Create diameter of binary tree.cpp

---
+++ amazon/diameter of binary tree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int heightModified(TreeNode<int> *root, int &maxi){
+    if (root == NULL){
+        return 0;
+    }
+    int lh = heightModified(root->left, maxi);
+    int rh = heightModified(root->right, maxi);
+    maxi = max(maxi, lh + rh);
+    
+    return max(lh , rh) + 1;
+}
+
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+    int maxi = 0;
+	heightModified(root, maxi);
+    return maxi;
+}
